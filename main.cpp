4a63a81a70de256887683a68a233b8f9c3a9460b
unknown <Student@SHSOBSS011.wf.uct.ac.za>
2015-09-13 02:27:00 +0200

    Solution to Break The Rules

--- main.cpp
+++ main.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int* twoNumbers(int myArray[])
+struct myclass {
+  bool operator() (int i,int j) { return (i<j);}
+} myObject;
+
+int* twoNumbers(int myArray[], int arrayLen, vector<int>* myResult)
 {
-    return myArray;
+    vector<int> myArray2;
+    for (int count1=0; count1 < arrayLen; count1++)
+    {
+        for (int count2=(count1+1); count2 < arrayLen; count2++)
+        {
+            int mySum = myArray[count1]+myArray[count2];
+            for (int count3=0; count3 < arrayLen; count3++)
+            {
+
+                if (myArray[count3] == mySum)
+                {
+                    bool isAlreadyThere = false;
+                    for (int count4 = 0; count4 < myArray2.size(); count4++)
+                        if (myArray2[count4] == mySum)
+                            isAlreadyThere = true;
+                    if (!isAlreadyThere)
+                        myArray2.push_back(mySum);
+                }
+            }
+        }
+    }
+
+    for (int count1=0; count1 < myArray2.size(); count1++)
+        myResult->push_back(myArray2[count1]);
+    return 0;
+
 }
 
 int main()
 {   int myNumbers[] = {1,2,3};
-    int* myNum = twoNumbers(myNumbers);
     int arrayLength = (sizeof(myNumbers)/sizeof(myNumbers[0]));
-    for (int count=0; count < arrayLength ; count++)
+    vector<int> myResult;
+    twoNumbers(myNumbers, arrayLength, &myResult);
+    sort (myResult.begin(), myResult.end(), myObject);
+    for (int count=0; count < myResult.size() ; count++)
     {
-        if (count < (arrayLength-1))
-            cout << myNum[count] << ",";
+        if (count < (myResult.size()-1))
+            cout << myResult[count] << ",";
         else
-            cout << myNum[count] << endl;
+            cout << myResult[count] << endl;
     }
 }
